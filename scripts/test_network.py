4f3594208958a53721c0cf43a6e545edebb51fa7
Robin177 <39666335+Robin177@users.noreply.github.com>
2020-05-16 21:08:30 +0800

    16改成17

--- scripts/test_network.py
+++ scripts/test_network.py
@@ -53,7 +53,7 @@ class TestNetwork:
         self.network_page.click_first_network()
         self.network_page.click_3g_network()	
 		
-	def test_mobile_network_16g(self):
+	def test_mobile_network_17g(self):
         self.network_page.click_more()
         self.network_page.click_network()
         self.network_page.click_first_network()
